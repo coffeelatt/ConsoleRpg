6bffec50876f6622584824828d5119bcef646e28
coffeelatt <ssongcoffee@gmail.com>
2025-04-08 09:42:36 +0900

    1

---
+++ ConsoleRpg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleRpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
