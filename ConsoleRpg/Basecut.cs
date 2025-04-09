96cbc1e23100f35fc42aed8e3bd358b97ca18b41
coffeelatt <ssongcoffee@gmail.com>
2025-04-09 15:20:18 +0900

    [Feat]게임 컷 동작 제작
    게임 클래스를 구현
    여러 씬들을 추가할수 있는 기틀 마련

---
+++ ConsoleRpg/Basecut.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpg
+{
+   public abstract class Basecut
+    {
+
+        public abstract void Render();
+       
+        public abstract void Input();
+
+
+
+        public abstract void Update();
+
+        public abstract void Result();
+       
+
+
+
+
+    }
+}
---
+++ ConsoleRpg/Game.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleRpg.cut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpg
+{
+    public class Game
+    {
+        private static Dictionary<string, Basecut> cutDic;
+        private static Basecut window;
+
+        private static bool gameover;
+
+        public static void Start()
+        { //게임의 초기설정
+        gameover=false;
+            //씬 설정
+            cutDic = new Dictionary<string, Basecut>();
+            cutDic.Add("Title", new Title());
+
+            window = cutDic["Title"];
+        }
+
+        public static void Run()
+        {
+            Start();
+            while (gameover==false)
+            {
+                Console.Clear();
+                
+                window.Render();
+                window.Input();
+                window.Update();
+                window.Result();
+
+            }
+            End();
+        }
+        public static void End()
+        { //게임의 종료
+            
+        }
+
+
+
+    }
+}
